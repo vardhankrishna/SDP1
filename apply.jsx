61d029c163c083e77d6b1ee1ec35077fe381df1e
vardhankrishna <90866305+vardhankrishna@users.noreply.github.com>
2021-11-10 15:37:33 +0530

    Create apply.jsx

---
+++ apply.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Apply = () => {
+  return (
+    <div className="Apply ">
+      <div className="container">
+        <div className="Apply__text">
+          <div>
+            <h1>Buy Insurance Plans-Compare & save Big .</h1>
+            <div className="Apply__btn">
+              <a href="" className="btn btn-smart">
+                Apply NOW
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Apply;
