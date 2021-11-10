85601e935c794cf1e855f283ceadfa88d137813c
vardhankrishna <90866305+vardhankrishna@users.noreply.github.com>
2021-11-10 15:31:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./Headers";
+import About from "./About";
+import Apply from "./Apply";
+import Clients from "./Clients";
+import Policy from "./Policy";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Apply />
+      <Clients />
+      <Policy />
+      
+    </div>
+  );
+}
+
+export default App;
